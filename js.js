57ac0abb3699786de5bd919760c556f845fa4808
dorotaurb <urbanska.dorota@yahoo.pl>
2016-02-17 20:52:04 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,16 +1,37 @@
 $(document).ready(function() {
 $('#proj-1').mouseover(function() {
-  $('.img-title').css("visibility","visible");
+  $('#img-title1').css("visibility","visible");
 });
-
 $('#proj-1').mouseout(function() {
-  $('.img-title').css("visibility","hidden");
-});
-  });
-$('#proj-2').mouseover(function() {
-  $('.img-title').css("visibility","visible");
+  $('#img-title1').css("visibility","hidden");
 });
 
+$('#proj-2').mouseover(function() {
+  $('#img-title2').css("visibility","visible");
+});
 $('#proj-2').mouseout(function() {
-  $('.img-title').css("visibility","hidden");
+  $('#img-title2').css("visibility","hidden");
+});
+
+$('#proj-3').mouseover(function() {
+  $('#img-title3').css("visibility","visible");
+});
+$('#proj-3').mouseout(function() {
+  $('#img-title3').css("visibility","hidden");
+});
+
+$('#proj-4').mouseover(function() {
+  $('#img-title4').css("visibility","visible");
+});
+$('#proj-4').mouseout(function() {
+  $('#img-title4').css("visibility","hidden");
+});
+
+$('#proj-5').mouseover(function() {
+  $('#img-title5').css("visibility","visible");
+});
+$('#proj-5').mouseout(function() {
+  $('#img-title5').css("visibility","hidden");
+});
+  
 });

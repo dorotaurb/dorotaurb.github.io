56c16d205e134495d2d8c3825ef562068e0b66dd
dorotaurb <urbanska.dorota@yahoo.pl>
2016-02-17 21:03:02 +0100

    Update js.js

--- js.js
+++ js.js
@@ -35,3 +35,15 @@ $('#proj-5').mouseout(function() {
 });
   
 });
+
+$(document).ready(function() {
+        $(window).scroll( function(){
+            $('.jumbotron').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},300);
+            }
+        }); 
+    });
+});

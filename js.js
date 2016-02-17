208b2fbd278e9e0a56fd19c2617a53be7a283388
dorotaurb <urbanska.dorota@yahoo.pl>
2016-02-17 21:13:23 +0100

    Update js.js

--- js.js
+++ js.js
@@ -37,7 +37,7 @@ $('#proj-5').mouseout(function() {
 
         $(window).scroll( function(){
             $('.jumbotron').each( function(i){
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight() / 2;
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             if( bottom_of_window > bottom_of_object ){
                 $(this).animate({'opacity':'1'},'slow');

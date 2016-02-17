2390f0a7b85b3959426af348c46af5829f080fdc
dorotaurb <urbanska.dorota@yahoo.pl>
2016-02-17 14:47:36 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-$('.col-sm-7').hover(
+$('#proj-1,#proj-2,#proj-3,#proj-4,#proj-5').hover(
   function() {
     $(this).find('.img-title').fadeOut(250);
   }, function() {
